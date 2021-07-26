cc5e52ba42c1a13728aad868c4b6aa2a2c1da813
Kisei Tanaka <1595661320@mil>
2021-07-25 15:54:55 -1000

    ok

--- scripts/6_Plot_Entire_Data.R
+++ scripts/6_Plot_Entire_Data.R
@@ -4,9 +4,13 @@ library(dplyr)
 
 rm(list = ls())
 
-island = "Lanai"
+# choose variable...
+variable = c("SST_CRW_Monthly", "Chlorophyll_A_ESAOCCCI_Monthly")[1]
 
-load(paste0('outputs/', island, '_raw_SST_CRW_Monthly.RData'))
+# choose island...
+island = c("Hawaii", "Kauai", "Kaula", "Lanai", "Maui", "Molokai", "Niihau", "Oahu")[1]
+
+load(paste0('outputs/', island, '_raw_', variable, '.RData'))
 df_i = df_i %>% subset(Island_Sector != "W")
 
 species = unique(df_i$Sp_Common_Name)
@@ -33,26 +37,49 @@ for (is in 1:length(island_sectors)) {
   library(data.table)
   df = setDT(df, keep.rownames = TRUE)[]
 
-  df = data.frame(date = df$rn, mean_SST = df$n)
+  df = data.frame(date = df$rn, mean = df$n)
 
-  df$correct_time_step = seq(as.Date("1985/1/1"), by = "month", length.out = 435)
+  if (variable == "SST_CRW_Monthly") {
+
+    df$correct_time_step = seq(as.Date("1985/1/1"), by = "month", length.out = dim(df)[1])
+
+  } else {
+
+    df$correct_time_step = seq(as.Date("1997/9/1"), by = "month", length.out = dim(df)[1])
+
+  }
+
 
   df$year = substr(df$correct_time_step, 1, 4)
   df$month = substr(df$correct_time_step, 6, 7)
 
   df$sector = island_sectors[is]
 
-  df = df %>% select(year, month, mean_SST, sector)
+  df = df %>% select(year, month, mean, sector)
 
   df_all = rbind(df_all, df)
 
 }
 
+# plot by month
 df_all %>%
-  ggplot(aes(month, mean_SST, color = year, group = year)) +
+  ggplot(aes(month, mean, color = year, group = year)) +
   geom_point() +
   geom_line() +
   facet_wrap(~sector) +
   scale_color_manual(values = matlab.like(length(unique(df_all$year))), "") +
   ggdark::dark_theme_minimal() +
   ggtitle(island)
+
+# plot by year
+df_all %>%
+  group_by(year, sector) %>%
+  summarise(mean = mean(mean)) %>%
+  ggplot(aes(year, mean, color = sector, group = sector)) +
+  geom_point() +
+  geom_line() +
+  scale_x_discrete(breaks = round(seq(min(df_all$year), max(df_all$year), by = 4), 1)) +
+  scale_color_manual(values = matlab.like(length(unique(df_all$sector))), "") +
+  ggdark::dark_theme_minimal() +
+  ggtitle(island)
+
