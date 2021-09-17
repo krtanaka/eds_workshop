9fdfb262930058a8759eb27071659edea891f1c8
Kisei Tanaka <kisei.tanaka@gmail.com>
2021-09-16 16:40:59 -1000

    ok

--- scripts/8_Normal_Abnormal.R
+++ scripts/8_Normal_Abnormal.R
@@ -9,85 +9,101 @@ rm(list = ls())
 variable = c("SST_CRW_Monthly", "Chlorophyll_A_ESAOCCCI_Monthly")[1]
 
 # choose island...
-island = c("Hawaii", "Kauai", "Kaula", "Lanai", "Maui", "Molokai", "Niihau", "Oahu")[1]
+islands = c("Hawaii", "Kauai", "Kaula", "Lanai", "Maui", "Molokai", "Niihau", "Oahu")
 
-# import full environmental data
-load(paste0('outputs/', island, '_raw_', variable, '.RData'))
+df_env_MHI = NULL
 
-island_sectors = unique(df_i$Island_Sector)
+for (i in 1:length(islands)) {
 
-df_all = NULL
+# i = 1
 
-for (is in 1:length(island_sectors)) {
+  # import full environmental data
+  load(paste0('outputs/', islands[i], '_raw_', variable, '.RData'))
 
-  # is = 1
+  island_sectors = unique(df_i$Island_Sector)
 
-  df = df_i %>% subset(Island_Sector == island_sectors[is])
+  df_all = NULL
 
-  df = as.data.frame(t(df))
+  for (is in 1:length(island_sectors)) {
 
-  df = df[-(1:6), , drop = FALSE] #remove first 6 rows
+    # is = 1
 
-  df[colnames(df)] <- sapply(df[colnames(df)],as.character)
-  df[colnames(df)] <- sapply(df[colnames(df)],as.numeric)
+    df = df_i %>% subset(Island_Sector == island_sectors[is])
 
-  sapply(df, class)
+    df = as.data.frame(t(df))
 
-  df$n = rowMeans(df[,c(1:dim(df)[2])], na.rm = T)
+    df = df[-(1:6), , drop = FALSE] #remove first 6 rows
 
-  library(data.table)
-  df = setDT(df, keep.rownames = TRUE)[]
+    df[colnames(df)] <- sapply(df[colnames(df)],as.character)
+    df[colnames(df)] <- sapply(df[colnames(df)],as.numeric)
 
-  df = data.frame(date = df$rn, mean = df$n)
+    sapply(df, class)
 
-  if (variable == "SST_CRW_Monthly") {
+    df$n = rowMeans(df[,c(1:dim(df)[2])], na.rm = T)
 
-    df$correct_time_step = seq(as.Date("1985/1/1"), by = "month", length.out = dim(df)[1])
+    library(data.table)
+    df = setDT(df, keep.rownames = TRUE)[]
 
-  } else {
+    df = data.frame(date = df$rn, mean = df$n)
 
-    df$correct_time_step = seq(as.Date("1997/9/1"), by = "month", length.out = dim(df)[1])
+    if (variable == "SST_CRW_Monthly") {
+
+      df$correct_time_step = seq(as.Date("1985/1/1"), by = "month", length.out = dim(df)[1])
+
+    } else {
+
+      df$correct_time_step = seq(as.Date("1997/9/1"), by = "month", length.out = dim(df)[1])
+
+    }
+
+
+    df$year = substr(df$correct_time_step, 1, 4)
+    df$month = substr(df$correct_time_step, 6, 7)
+
+    df$sector = island_sectors[is]
+
+    df = df %>% select(year, month, mean, sector)
+
+    df_all = rbind(df_all, df)
 
   }
 
+  env_q = df_all %>%
+    group_by(year, month) %>%
+    mutate(q10 = quantile(mean, probs = 0.1),
+           q90 = quantile(mean, probs = 0.9))
 
-  df$year = substr(df$correct_time_step, 1, 4)
-  df$month = substr(df$correct_time_step, 6, 7)
+  rm(df_all)
 
-  df$sector = island_sectors[is]
+  isl = islands[i]
 
-  df = df %>% select(year, month, mean, sector)
 
-  df_all = rbind(df_all, df)
+  # import species-catch data
+  load("outputs/Timeseries_2021-07-29.Rdata")
+  SM[SM == -9991] <- NA
+  setDT(SM)[, paste0("DATE_R", 1:3) := tstrsplit(DATE_R, "-")]
+  colnames(SM)[36:38] = c("year", "month", "day")
+
+  df = SM %>%
+    subset(ISLAND == isl) %>%
+    group_by(year, month, day, SP, ISLAND, SECTOR) %>%
+    summarise(sst = mean(mean_SST_CRW_Daily_MO03, na.rm = T))
+
+  df$catch = 1
+  df = df %>% na.omit()
+
+  colnames(df)[4:6] = c("species", "island", "sector")
+
+  df_env = merge(df, env_q)
+
+  df_env$abnormal = ifelse(df_env$sst > df_env$q10 & df_env$sst < df_env$q90, "No", "Yes")
+  table(df_env$abnormal)
+
+  df_env_MHI = rbind(df_env_MHI, df_env)
+
+  print(isl)
+
 
 }
 
-env_q = df_all %>%
-  group_by(year, month) %>%
-  mutate(q10 = quantile(mean, probs = 0.1),
-         q90 = quantile(mean, probs = 0.9))
-
-rm(df_all)
-
-isl = island
-
-
-# import species-catch data
-load("outputs/Timeseries_2021-07-29.Rdata")
-SM[SM == -9991] <- NA
-setDT(SM)[, paste0("DATE_R", 1:3) := tstrsplit(DATE_R, "-")]
-colnames(SM)[36:38] = c("year", "month", "day")
-
-df = SM %>%
-  group_by(year, month, day, SP, ISLAND, SECTOR) %>%
-  summarise(sst = mean(mean_SST_CRW_Daily_MO03, na.rm = T))
-
-df$catch = 1
-df = df %>% na.omit()
-
-colnames(df)[4:6] = c("species", "island", "sector")
-
-df_env = merge(df, env_q)
-
-df_env$abnormal = ifelse(df_env$sst > df_env$q10 & df_env$sst < df_env$q90, "No", "Yes")
-table(df_env$abnormal)
+readr::write_csv(df_env_MHI, file = 'outputs/df_env_MHI.csv')
